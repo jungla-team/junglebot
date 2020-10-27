ffce0d83e2500cfa61cc4258d40b62e2dc3e0f7f
jungla-team <you@example.com>
2020-10-27 10:42:09 +0100

    update 27102020

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ NEW_VERSION=`grep "VERSION=\"" ${DIR_SRC}/bot.py | cut -d'=' -f2 | sed "s/\"//g"
 NEW_VERSION=$(eval echo $NEW_VERSION)
 
 if [ "$1" == "vti" ]; then
-  DIR_IPK_SRC=$DIR_SRC/package-vti/
+  DIR_PKG_BUILD=$DIR_SRC/package-vti/
   TYPE="vti"
 else
-  DIR_IPK_SRC=$DIR_SRC/package/
+  DIR_PKG_BUILD=$DIR_SRC/package/
   TYPE="all"
 fi
 
@@ -19,22 +19,16 @@ PKG_NAME=junglebot_${NEW_VERSION}_all
 
 echo "Building package $PKG_NAME"
 
-DIR_PKG_BUILD=$DIR_IPK_TMP/$PKG_NAME
-rm -rf $DIR_PKG_BUILD
-mkdir -p $DIR_PKG_BUILD
-
-cp -rp $DIR_IPK_SRC/* $DIR_PKG_BUILD/
 newLine="Version: ${NEW_VERSION}"
 sed -i "2s/.*/${newLine}/" $DIR_PKG_BUILD/CONTROL/control
 
 DIR_PKG_DEST=$DIR_PKG_BUILD/usr/bin/junglebot
-mkdir -p $DIR_PKG_DEST
 if [ "$1" == "vti" ]; then
   REQ_FILE='requirements-vti.txt'
 else
   REQ_FILE='requirements.txt'
 fi
-cp -rf $DIR_SRC/bot.py $DIR_SRC/$REQ_FILE $DIR_SRC/amigos.cfg $DIR_SRC/parametros.py $DIR_SRC/locales $DIR_SRC/images $DIR_PKG_DEST
+cp -rf $DIR_SRC/bot.py $DIR_SRC/$REQ_FILE $DIR_SRC/amigos.cfg $DIR_SRC/parametros.py $DIR_SRC/locales $DIR_SRC/images $DIR_SRC/CHANGELOG.md $DIR_SRC/README.md $DIR_SRC/LICENSE $DIR_PKG_DEST
   
 ipkg-build ${DIR_PKG_BUILD}
 
---
+++ package-vti/usr/bin/junglebot/README.md
@@ -0,0 +1,5 @@
+# Junglebot - Bot de telegram de jungle-team
+
+Bot de telegram válido para imágenes: OpenATV, OpenPLI, OpenSPA y VTI
+
+Información completa del bot en: https://jungle-team.com/junglebot-2-5-0-controla-enigma2-con-telegram/
